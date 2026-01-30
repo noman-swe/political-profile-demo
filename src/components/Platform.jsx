c58021682b8f930b9201a3e4c5cd3f754b85d481
noman <noman.swe130@gmail.com>
2026-01-31 04:43:39 +0600

    --

--- src/components/Platform.jsx
+++ src/components/Platform.jsx
@@ -9,29 +9,30 @@ const Platform = () => {
           সমস্যা এবং <span className="text-primary">প্ল্যাটফর্ম</span>
         </h2>
         <p className="mb-12 opacity-70">
-          বিএনপি-র ৩১-দফা সংস্কার এজেন্ডার সাথে সামঞ্জস্যপূর্ণ আমাদের মূল
-          অবস্থান:
+          {/* বিএনপি-র ৩১-দফা সংস্কার এজেন্ডার সাথে সামঞ্জস্যপূর্ণ আমাদের মূল
+          অবস্থান: */}
+          আমার সংস্কার এজেন্ডা ও অঙ্গিকার সমূহ-
         </p>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {platforms.map((item, idx) => {
             const Icon = item.icon;
             return (
-            <div
-              key={idx}
-              className="bg-primary text-white p-6 rounded-xl space-y-4"
-            >
-              <div className="flex items-center gap-3">
-                <Icon className="text-white mb-4 w-8 h-8" />
-                <h3 className="font-bold">{item.title}</h3>
+              <div
+                key={idx}
+                className="bg-primary text-white p-6 rounded-xl space-y-4"
+              >
+                <div className="flex items-center gap-3">
+                  <Icon className="text-white mb-4 w-8 h-8" />
+                  <h3 className="font-bold">{item.title}</h3>
+                </div>
+                <ul className="text-sm space-y-2 opacity-90 list-disc pl-5">
+                  {item.points.map((point, i) => (
+                    <li key={i}>{point}</li>
+                  ))}
+                </ul>
               </div>
-              <ul className="text-sm space-y-2 opacity-90 list-disc pl-5">
-                {item.points.map((point, i) => (
-                  <li key={i}>{point}</li>
-                ))}
-              </ul>
-            </div>
-          );
-        })}
+            );
+          })}
         </div>
       </div>
     </section>
